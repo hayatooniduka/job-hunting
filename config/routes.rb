15f49e9b274f4edf49de418f1c3da953792e2868
hayato.oniduka <hayato.onika@gmail.com>
2021-10-26 14:35:39 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :create, :update]
  end
